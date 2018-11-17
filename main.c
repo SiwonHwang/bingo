3ee8e2e8352ee7d54c7af951d1741b1e271216ec
SiwonHwang <kimshluv@naver.com>
2018-11-17 23:00:13 +0900

    11/17

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N 4
-#define M 3
+#define N 4 // 빙고 테이블 기호상수  
+#define M 3 // 빙고 이기는 조건 기호상수  
 
 /*
 initiate_bingo() : 빙고 테이블을 초기에 만들어주는 함수 
@@ -14,32 +14,62 @@ process_bingo() : 선택된 숫자를 입력받아서 빙고 테이블 칸을 채움
 count_bingo() : 빙고 테이블이 채운 가로/세로/대각선 줄 수를 계산하여 반환 
 */
 
-
-int initiate_bingo();
-int ptint_bingo();
-int get_number_byMe();
-int get_number_byCom();
-int process_bingo();
-int count_bingo();
-
-
 int main(void){
 	
-	int Me[N][N]; //내 빙고 테이블 
-	int Com[N][N]; //컴퓨터 빙고 테이블 
+	int bingoMe[N][N]; //내 빙고 테이블 
+	int bingoCom[N][N]; //컴퓨터 빙고 테이블 
 	int MeNum; //내가 선택한 숫자 
 	int ComNum; //컴퓨터가 선택한 숫자 
-	int i, j;
 	
-	srand(time(NULL));
-		for (i=0; i<N; i++)
-		{
-			for(j=0; j<N; j++)
-			{
-				printf("%d ", 1+rand()%(N*N)); 
-			}
-		printf("\n");
-		}
+	void initiate_bingo(N, bingoMe);
+	void print_bingo(N, bingoMe);
 	
 	return 0;
 }
+
+
+void initiate_bingo(int n, bingotable[n][n]){
+	
+	int bingotable[n][n];
+	int i, j;
+	int max = n*n
+	
+	srand((unsigned)time(NULL));
+	
+	for (i=0; i<max; i++){
+		bingMe[0][i] = rand()%max;
+		
+		for (j=0; j<i; j++){
+			if (bingotable[0][j] == bingotable[0][i]){
+				i--;
+				break;
+			}
+		}
+	}
+	
+}
+	
+void print_bingo(int n, bingotable[n][n]){
+	
+	int bingotable[n][n];
+	int i, j;
+	
+	for (i=0; i<n; i++){
+		for (j=0; j<n; j++){
+			printf("%d ", bingotable[i][j]);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
